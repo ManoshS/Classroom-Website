d2d5aa4e384dabac7a51b1431c3a09f30ad0d392
Manosh <smanosh73@gmail.com>
2024-08-12 15:12:29 +0530

    Small changes

--- StudentManagementBackend/config/db.js
+++ StudentManagementBackend/config/db.js
@@ -1,11 +1,7 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+const mysqlurl=`mysql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`
+const pool = mysql.createPool(mysqlurl);
 
 module.exports = pool.promise();
